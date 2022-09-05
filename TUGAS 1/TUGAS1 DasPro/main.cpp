e5ea371176456da4f17a27151dfb202634413123
Muhammad Iqbal Septiawan <112881751+MuhammadIqbalSeptiawan@users.noreply.github.com>
2022-09-05 23:14:18 +0700

    Add files via upload

---
+++ TUGAS 1/TUGAS1 DasPro/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Muhammad Iqbal Septiawan" << endl;
+    cout << "A11.2022.14114" << endl;
+    cout << "Graha Permata Surya blok A11, Ngadirgo" << endl;
+    cout << "SMAN 05 Kota Madiun" << endl;
+    return 0;
+}

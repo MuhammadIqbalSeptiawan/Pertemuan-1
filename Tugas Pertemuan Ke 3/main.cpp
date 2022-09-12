ef90dd7eb1e39bbde97a1801aa0e80aebe22104b
Muhammad Iqbal Septiawan <112881751+MuhammadIqbalSeptiawan@users.noreply.github.com>
2022-09-12 20:15:18 +0700

    Add files via upload

---
+++ Tugas Pertemuan Ke 3/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Muhammad Iqbal Septiawan" << endl;
+    /*printf ("iqbal")
+    Muhammad Iqbal Septiawan
+    A11.2022.14114
+    A11.4104
+    */
+
+    int u = 17;
+    float t = 172.5;
+    char b = 'M';
+    //string ub = XXL
+    cout << "Umur saya:" << u;
+    cout << "\nTinggi saya:" << t;
+    cout << "\nUkuran baju saya:" << b;
+
+    return 0;
+}

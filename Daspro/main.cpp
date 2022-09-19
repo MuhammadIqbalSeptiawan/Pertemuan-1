ef6992702ab18b873df798f1b401a0240d49a7c7
Muhammad Iqbal Septiawan <112881751+MuhammadIqbalSeptiawan@users.noreply.github.com>
2022-09-19 20:26:17 +0700

    Add files via upload

---
+++ Daspro/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+
+    float r;
+    float phi = 3.14;
+    float l;
+    cout << "jari jari =" << endl;
+    cin >> r;
+    l = phi * r * r;
+    cout << "luas lingkaran " << r << " meter adalah: " << l << "cm2";
+
+/*
+    float L;
+    float p;
+    float l;
+
+    cout <<"panjang = " << endl;
+    cin >> p;
+    cout << "lebar= " << endl;
+    cin >> l;
+    L = p * l;
+    cout << "luas persegi panjang tersebut adalah " << L << " cm2";
+    */
+
+
+
+
+
+
+
+    cout << "" << endl;
+    return 0;
+}
